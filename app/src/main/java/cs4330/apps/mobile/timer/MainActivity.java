af4a0694aecb7d6f3700e14056103ee00a8014a4
Oscar Ricaud <oiricaud@miners.utep.edu>
2017-01-26 09:50:23 -0700

    Update MainActivity.java

--- app/src/main/java/cs4330/apps/mobile/timer/MainActivity.java
+++ app/src/main/java/cs4330/apps/mobile/timer/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Chronometer;
 
 /** An Android Application that allows users to use a timer.
  * @author Oscar I. Ricaud
- * Homework 1 - Note Applets have been outdated... used Android Studio to develop the app.
+ * CS 4330 Homework 1 - Note Applets have been outdated... used Android Studio to develop the app.
  * @version 1.0
  */
 public class MainActivity extends AppCompatActivity {
